7b3a725fe064630273ae2c8348e6c98e580d7619
ToRong31 <100920474+ToRong31@users.noreply.github.com>
2025-04-04 14:52:39 +0700

    Update nmfhtr.py

--- rfm/rfm/nmfhtr.py
+++ rfm/rfm/nmfhtr.py
@@ -25,7 +25,7 @@ def asm_nmf_fn(samples, map_fn, rank=10, max_iter=100, init_mode='nndsvd', verbo
         kernel_matrix,layers, k_list, theta_list, max_iter, init_mode,lambda_sparseness=0.1
     )
     W,H,S=W_list[-1],H_list[-1],S_list[-1]
-    return torch.from_numpy(W).float().to(device), torch.from_numpy(H).float().to(device), torch.from_numpy(S).to(device), norms
+    return torch.from_numpy(W).float().to(device), torch.from_numpy(H).float().to(device), torch.from_numpy(S).float().to(device), norms
 
 class KernelModel(nn.Module):
     '''Fast Kernel Regression using EigenPro iteration.'''
